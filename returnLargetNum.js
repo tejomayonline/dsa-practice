ef888250cd9889c432a9f8b56ca2e894e78fc8ed
tejomay.saha <tejomay.saha@cimpress.com>
2026-01-14 21:14:01 +0530

    returnLargestNum added

---
+++ returnLargetNum.js
@@ -0,0 +1,17 @@
+
+function returnLargestNum(arr) {
+    if (arr.length === 0) {
+        return null; // Return null for empty array
+    }
+    let largest = arr[0];
+    for ( let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+
+const numbers = [3, -1, 14, 2, 0, -5];
+console.log(returnLargestNum(numbers)); // Output: 14
